eb6b40e598fd23df8d9ca4173bf5331803682611
Pablo Professi <pablo.professi@mercadolibre.com>
2016-11-15 22:29:48 -0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ from . import extensions, resources
 def create_app(environment='dev'):
     app = Flask('booksurfer-api')
     app.config['DEBUG'] = True
-    app.log.setLevel('DEBUG') 
     CORS(app)
 
     app.config.from_pyfile('config/{}.py'.format(environment))
